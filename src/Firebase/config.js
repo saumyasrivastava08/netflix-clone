87bb8faa2eeac9cc8b825c71611f21a7e375c25b
Saumya Srivastava <57553757+saumyasrivastava08@users.noreply.github.com>
2021-07-17 21:04:36 +0530

    Update config.js

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,12 +1,14 @@
 const config = {
-  apiKey: "AIzaSyD5us61KTaFhvjF-oXd-yUZLG2_Y5Dpmgs",
-  authDomain: "netflix-clone-694ff.firebaseapp.com",
-  projectId: "netflix-clone-694ff",
-  storageBucket: "netflix-clone-694ff.appspot.com",
-  messagingSenderId: "335556352952",
-  appId: "1:335556352952:web:a78dc04f7bd2d9d835cd4d",
-  measurementId: "G-F8EGEVQ3QQ"
+  apiKey: "writeapi",
+  authDomain: "writeauthdomain",
+  projectId: "writepid",
+  storageBucket: "writestb",
+  messagingSenderId: "writemsgid",
+  appId: "writeappid",
+  measurementId: "writemesumeremntid"
 
 }
 
 export default config;
+
+//Write all the configuration from Firebase App .
